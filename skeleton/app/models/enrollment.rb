05439b6a27b4d8aee66e8d8007a42d374cb78d62
Leo Liao <ducleoliao@gmail.com>
2017-06-14 11:59:02 -0700

    Add associations and start URLShortener rails app

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -1,2 +1,16 @@
 class Enrollment < ActiveRecord::Base
+  validates :course_id, presence: true, uniqueness: true
+  validates :student_id, presence: true, uniqueness: true
+
+  belongs_to :user, {
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: 'User'
+  }
+
+  belongs_to :course, {
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: 'Course'
+  }
 end
